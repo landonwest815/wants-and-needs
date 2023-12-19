59356e77ea6f20614dea45842f3a175384c3fcda
Landon West <landonwest815@gmail.com>
2023-12-19 16:00:18 -0700

    Removing files that should have been in .gitignore

---
+++ Wants+Needs/Views/NeedsView.swift
@@ -0,0 +1,18 @@
+//
+//  NeedsView.swift
+//  Wants+Needs
+//
+//  Created by Landon West on 12/19/23.
+//
+
+import SwiftUI
+
+struct NeedsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NeedsView()
+}
---
+++ Wants+Needs/Wants+Needs/Wants_NeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wants_NeedsApp.swift
+//  Wants+Needs
+//
+//  Created by Landon West on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Wants_NeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
