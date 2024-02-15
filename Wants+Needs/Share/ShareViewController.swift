c5761c063fdc651741922c51afb9ce0508afc87b
Landon West <landonwest815@gmail.com>
2024-02-15 14:41:44 -0700

    Setup ShareSheet

---
+++ Wants+Needs/Share/ShareViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ShareViewController.swift
+//  Share
+//
+//  Created by Landon West on 2/15/24.
+//
+
+import UIKit
+import Social
+
+class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
