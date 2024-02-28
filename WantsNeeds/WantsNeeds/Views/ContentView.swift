f46c94ecb361cb4d03d6d391bc8006366af246e7
Landon West <landonwest815@gmail.com>
2024-02-28 12:52:11 -0700

    App Submission

--- WantsNeeds/WantsNeeds/Views/ContentView.swift
+++ WantsNeeds/WantsNeeds/Views/ContentView.swift
@@ -25,6 +25,7 @@ struct ContentView: View {
                 OnboardView()
             }
         }
+        .preferredColorScheme(.dark)
         .onAppear() {
             handleUserSettings()
         }
